5cbe63e363d50170ed3d4f5b891df9e84bb5ca13
Ahmmed Abir <ahmmedabir9@gmail.com>
2022-09-01 10:03:41 +0600

    update boilerplate packages

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,25 +1,21 @@
-const { StatusCodes } = require("http-status-codes");
-const { response } = require("../utils/response");
-const { BlobServiceClient } = require("@azure/storage-blob");
+const { StatusCodes } = require('http-status-codes')
+const { response } = require('../utils/response')
+// const { BlobServiceClient } = require("@azure/storage-blob");
+const { v4: uuidv4 } = require('uuid')
 
-const blobString = ""; // Blob Connection String
+// const blobString = ""; // Blob Connection String
 
 //Upload Files to Local Directory
 const uploadFile = async (req, res) => {
   if (req.files === undefined || !req.files.image) {
-    let msg = "No file found !";
-    return response(res, StatusCodes.BAD_REQUEST, false, null, msg);
+    let msg = 'No file found !'
+    return response(res, StatusCodes.BAD_REQUEST, false, null, msg)
   }
 
   try {
-    const file = req.files.image;
-    const fileName = `${
-      Math.floor(Math.random() * 1000000000).toString(36) +
-      Math.floor(Math.random() * 1000000000).toString(36) +
-      Math.floor(Math.random() * 1000000000).toString(36) +
-      Math.floor(Math.random() * 1000000000).toString(36)
-    }${file.name}`;
-    const filePath = `${fileName}`;
+    const file = req.files.image
+    const fileName = `${uuidv4()}${file.name}`
+    const filePath = `${fileName}`
     file.mv(`uploads/${filePath}`, (err) => {
       if (err) {
         return response(
@@ -27,28 +23,28 @@ const uploadFile = async (req, res) => {
           StatusCodes.BAD_REQUEST,
           false,
           { err: err },
-          "Could not upload"
-        );
+          'Could not upload',
+        )
       }
-      const photoURL = filePath;
+      const photoURL = filePath
       return response(
         res,
         StatusCodes.ACCEPTED,
         true,
         { fileName: photoURL },
-        null
-      );
-    });
+        null,
+      )
+    })
   } catch (error) {
     return response(
       res,
       StatusCodes.INTERNAL_SERVER_ERROR,
       false,
       {},
-      error.message
-    );
+      error.message,
+    )
   }
-};
+}
 
 //Upload Files to Microsoft Azure Blob Storage
 // const uploadFile = async (req, res) => {
@@ -62,12 +58,7 @@ const uploadFile = async (req, res) => {
 //   const containerName = ""; //Blob Container Name
 //   const containerClient = blobServiceClient.getContainerClient(containerName);
 
-//   const randomString =
-//     Math.random().toString(36).substring(2, 15) +
-//     Math.random().toString(36).substring(2, 15) +
-//     Math.random().toString(36).substring(2, 15) +
-//     Math.random().toString(36).substring(2, 15);
-//   const blobName = `${randomString}.` + file.name.split(".").pop();
+//   const blobName = `${uuidv4()}.` + file.name.split(".").pop();
 
 //   try {
 //     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
@@ -88,4 +79,4 @@ const uploadFile = async (req, res) => {
 
 module.exports = {
   uploadFile,
-};
+}
