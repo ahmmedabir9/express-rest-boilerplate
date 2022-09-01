558b82f3b1ecd6abe459ff8ddec27c3366781d54
Md Monir Hossain Showrav <61703809+shourov-sorkar@users.noreply.github.com>
2022-09-01 10:18:10 +0600

    :sparkles: Added Html template for Server Running

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const { connect } = require('mongoose')
-const bodyParser = require('body-parser')
-const { database } = require('./src/config/database')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { connect } = require("mongoose");
+const bodyParser = require("body-parser");
+const { database } = require("./src/config/database");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(fileUpload())
-app.use(cors())
+app.use(fileUpload());
+app.use(cors());
 
 try {
   connect(
@@ -21,31 +21,31 @@ try {
       useFindAndModify: false,
     },
     () => {
-      console.log('Database Connected')
-    },
-  )
+      console.log("Database Connected");
+    }
+  );
 } catch (err) {
-  console.log('Database Connection Error')
+  console.log("Database Connection Error");
 }
 
-app.use(express.static(path.join(__dirname, '/uploads')))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "/uploads")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const authRoute = require('./src/routes/auth.routes')
-const fileRoute = require('./src/routes/file.routes')
-const userRoute = require('./src/routes/user.routes')
+const authRoute = require("./src/routes/auth.routes");
+const fileRoute = require("./src/routes/file.routes");
+const userRoute = require("./src/routes/user.routes");
 
-app.use('/user', userRoute)
-app.use('/file', fileRoute)
-app.use('/auth', authRoute)
+app.use("/user", userRoute);
+app.use("/file", fileRoute);
+app.use("/auth", authRoute);
 
-app.get('/', (req, res) => {
-  res.send(`RUNNING`)
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/view/serverRunning.html"));
+});
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('Server is Running on ' + port)
-})
+  console.log("Server is Running on " + port);
+});
